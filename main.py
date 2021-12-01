3ed813c7e1fcf82acbd87aeab5e6f5a2b4260cc0
Oishwarjya <ohitsmeoishee@gmail.com>
2021-12-01 16:07:03 +0530

    Issues excel analysed and added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pandas_profiling import ProfileReport
 
 df = pd.read_excel('competitor_daily_sales.xlsx', error_bad_lines=False)
 df2 = pd.read_excel('competitor_monthly_costs.xlsx', error_bad_lines=False)
-print(df)
+df3 = pd.read_excel('high_priority_usage_data.xlsx', error_bad_lines=False)
 
 #Generate a report
 profile = ProfileReport(df2)
@@ -13,3 +13,6 @@ profile.to_file(output_file="monthly_costs.html")
 
 profile = ProfileReport(df)
 profile.to_file(output_file="daily_sales.html")
+
+profile = ProfileReport(df3)
+profile.to_file(output_file="issues.html")
